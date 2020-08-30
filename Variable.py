01edfabb1e55dfe1ccfcbfb5f22661609d4e8173
Vip Patel <jvrapatel@gmail.com>
2020-08-30 09:24:35 -0400

    Initial commit

---
+++ Variable.py
@@ -0,0 +1,11 @@
+Supplier = "Google Cloud Platform"
+bid_amount = "$1.8MM USD"
+
+print ("The winner of the Capital One Cloud Services contract is " + Supplier + ".")
+print ("Capital One accepted a bid of " + bid_amount + " per year for five years.")
+print ("The bid by " + Supplier + " was accepted by Capital One on September 1, 2020.")
+print ("While the bid of " + bid_amount + " was not dispositive, it was one of many elements")
+print ("Capital One used to select a winning Supplier.")
+
+Supplier = "Amazon Web Services"
+print ("Capital One would like to thank " + Supplier + " for its services.")
